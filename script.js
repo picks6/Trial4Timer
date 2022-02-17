103009cbf7f3bea53bc5676a19fbacb8715875b2
Zach P <95437073+picks6@users.noreply.github.com>
2022-02-17 14:02:40 -0500

    added style buttons and hotkey function

--- script.js
+++ script.js
@@ -20,6 +20,12 @@ function countdown() {
 
 // Displays the message one second at a time
 function runTimer() {
+  window.addEventListener("keydown", function(e){
+    event.stopPropagation();
+    var code = e.keyCode;
+     if(code == 32){
+      addTime();   
+     }}) ;
   var addTimeBtn = document.getElementById('generate');
   addTimeBtn.addEventListener('click', addTime);
   timeRemaining = 1200;
@@ -48,3 +54,56 @@ function addTime(){
 
 var startBtn = document.getElementById('startBtn');
 startBtn.addEventListener('click',countdown);
+
+var bladesBtn = document.getElementById('violetblades');
+var masksBtn = document.getElementById('redmasks');
+var wingsBtn = document.getElementById('goldwings');
+var headerEl = document.getElementById('header');
+var timerBtn = document.getElementById('generate');
+
+bladesBtn.addEventListener('click',bladesify);
+masksBtn.addEventListener('click',masksify);
+wingsBtn.addEventListener('click',wingsify);
+
+function bladesify(){
+  headerEl.classList.toggle('bladesTheme');
+  startBtn.classList.toggle('bladesTheme');
+  timerBtn.classList.toggle('bladesTheme');
+  headerEl.classList.remove('wingsTheme');
+  headerEl.classList.remove('masksTheme');
+  startBtn.classList.remove('wingsTheme');
+  startBtn.classList.remove('masksTheme');
+  timerBtn.classList.remove('masksTheme');
+  timerBtn.classList.remove('wingsTheme');
+};
+
+function wingsify(){
+  headerEl.classList.toggle('wingsTheme');
+  startBtn.classList.toggle('wingsTheme');
+  timerBtn.classList.toggle('wingsTheme');
+  headerEl.classList.remove('bladesTheme');
+  headerEl.classList.remove('masksTheme');
+  startBtn.classList.remove('bladesTheme');
+  startBtn.classList.remove('masksTheme');
+  timerBtn.classList.remove('bladesTheme');
+  timerBtn.classList.remove('masksTheme');
+};
+
+function masksify(){
+  headerEl.classList.toggle('masksTheme');
+  startBtn.classList.toggle('masksTheme');
+  timerBtn.classList.toggle('masksTheme');
+  headerEl.classList.remove('wingsTheme');
+  headerEl.classList.remove('bladesTheme');
+  startBtn.classList.remove('bladesTheme');
+  startBtn.classList.remove('wingsTheme');
+  timerBtn.classList.remove('bladesTheme');
+  timerBtn.classList.remove('wingsTheme');
+};
+
+window.addEventListener("keydown", function(e){
+  event.stopPropagation();
+  var code = e.keyCode;
+   if(code == 16){
+    countdown();   
+   }}) ;
